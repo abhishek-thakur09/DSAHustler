e123cab0b883b065ff7ed89712f51b9606354401
Abhishek Thakur <abburajput7876@gmail.com>
2026-01-07 13:04:13 +0530

    added authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,43 @@
-import './App.css';
-import { Landing } from './coponents/Landing';
+import './App.css'
+import { Landing } from './coponents/Landing'
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { Signin } from './coponents/Signin';
+import { useEffect } from 'react';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAbno9__ueLY2jd6uOKrsMPT27iyKdS9aM",
+  authDomain: "dsahustler.firebaseapp.com",
+  projectId: "dsahustler",
+  storageBucket: "dsahustler.firebasestorage.app",
+  messagingSenderId: "556482804526",
+  appId: "1:556482804526:web:134614d93d5eb9d7b118bf",
+  measurementId: "G-1F295DLRS8"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
 
 
 function App() {
+
+useEffect(()=>{
+  onAuthStateChanged(auth, function(user){
+    if(user){
+      console.log("This is the user ", user);
+    }
+    else{
+      console.log("there is no loggedin User");
+    }
+  })
+},[]);
+
+
 
   return (
     <>
--- src/coponents/Signin.tsx
+++ src/coponents/Signin.tsx
@@ -2,6 +2,10 @@ import { getAuth, sendSignInLinkToEmail } from "firebase/auth";
 import { useState } from "react";
 
 
+import {signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+const provider = new GoogleAuthProvider();
+
 
 const actionCodeSettings = {
 
@@ -18,18 +22,31 @@ export const Signin = () => {
 
     async function onSignin(){
 
- await sendSignInLinkToEmail(auth, email, actionCodeSettings)
-                .then(() => {
-                window.localStorage.setItem("emailForSignIn", email);
-                alert("sent email")
-          
-        })
-                .catch((error) => {
-                    alert("email not sent")
-                const errorCode = error.code;
-                const errorMessage = error.message;
-               
-    });
+      signInWithPopup(auth, provider)
+             .then((result) => {
+              
+              // This gives you a Google Access Token. You can use it to access the Google API.
+              const credential = GoogleAuthProvider.credentialFromResult(result);
+              if(!credential){
+                return;
+              }
+              const token = credential.accessToken;
+              // The signed-in user info.
+              const user = result.user;
+              console.log(user);
+              
+              // IdP data available using getAdditionalUserInfo(result)
+              // ...
+            }).catch((error) => {
+              // Handle Errors here.
+              const errorCode = error.code;
+              const errorMessage = error.message;
+              // The email of the user's account used.
+              const email = error.customData.email;
+              // The AuthCredential type that was used.
+              const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
 }
 
   return <div>
